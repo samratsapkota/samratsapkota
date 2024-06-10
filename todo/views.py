7d3c63c60e6b58aecefe16699a778bbe567b65f1
Birat Sapkota <birat.sapkota@ekbana.info>
2024-06-10 17:59:15 +0545

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'todo/index.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'todo/update_task.html', context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+    
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+    
+    context = {'item': item}
+    return render(request, 'todo/delete.html', context)
